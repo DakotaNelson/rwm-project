6609c5a6f60e4f0382efc87ac3b6e1eb97cffb91
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-04-01 02:40:29 -0400

    Everything works now

--- temp_logger/temp_logger.ino
+++ temp_logger/temp_logger.ino
@@ -1,14 +1,22 @@
 #include <RWM.h>
 #include <SPI.h>
 
-#define DATA_BUFFER_SIZE  2
+void dumpData();
 
 RWM rwm = RWM();
 
-int tempPin = 2;
-unsigned int databuffer[DATA_BUFFER_SIZE];
-unsigned long count = 20;
+static int tempPin = 2;
+static int interval = 3; // in seconds
+/*
+ Given a 128 kbyte EEPROM and 2 bytes per record, 
+ this lets us log 3 second intervals for ~ 2 days and 5 hours.
+ At 5 seconds, memory will hold out for ~ 3 days, 16 hours and some change.
+*/
+
+int databuffer;
+unsigned long count;
 unsigned long address = 0;
+unsigned long temp_address = 0;
 
 
 void setup(){
@@ -17,41 +25,34 @@ void setup(){
 
 
 void loop(){
-  /*while(count > 20) {
-    logdata();
-    count--;
-    Serial.println(count);
-  }*/
-  if(!Serial) {}
   
-  address = 0;
-  for(count=20;count>0;count--) {
-    databuffer[0] = millis()/1000;
-    databuffer[1] = analogRead(tempPin);
-    rwm.EEPROMwriteInts(address, DATA_BUFFER_SIZE, databuffer);
-    Serial.print(databuffer[0]);
-    Serial.print(",");
-    Serial.print(databuffer[1]);
-    Serial.print(" written to ");
-    Serial.println(address);
-    address += DATA_BUFFER_SIZE;
-    delay(1000);
+  while(millis() < 30000) { // leave a 30 second bootup time during which to connect serial
+    if(Serial) { // if serial is connected during this time, dump the stored logs
+      dumpData();
+      while(true); // block forever, we only want to dump data this run, not log
+    } // else start logging
   }
   
-  address = 0;
-  for(count=20;count>0;count--) {
-    databuffer[0] = rwm.EEPROMreadInt(address);
-    address+=1;
-    databuffer[1] = rwm.EEPROMreadInt(address);
+  address = 1; // leave an int to store the number of records
+  while(true) { // log until unplugged
+    databuffer = analogRead(tempPin);
+    rwm.EEPROMwriteInt(address, databuffer); // log a record
+    rwm.EEPROMwriteInt(0,address); // at address 0, write the number of records we've logged so far
     address += 1;
-    Serial.print(databuffer[0]);
-    Serial.print(",");
-    Serial.print(databuffer[1]);
-    Serial.print(" read from ");
-    Serial.println(address);
+    delay(interval*1000); // delay some seconds
   }
-  
-  Serial.println("Done.");
-  //Serial.println(.0049*analogRead(tempPin)*100.0);
-  //delay(1000);
+}
+
+void dumpData() {
+  temp_address = 1; // start from one, we don't want to output the number of records
+  address = rwm.EEPROMreadInt(0); // at address 0 the number of records is stored
+  Serial.print("Number of records: ");
+  Serial.println(address);
+  for(count=address;count>0;count--) {
+    databuffer = rwm.EEPROMreadInt(temp_address);
+    temp_address+=1;
+    Serial.print(databuffer); // output everything in more or less a csv format
+    Serial.print(",");
+    delay(1); // don't scare the EEPROM
+  }
 }
